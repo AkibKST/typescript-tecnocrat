02b8048c2b027bb5662d52fed6640085027ae372
AkibKST <akibkst22@gmail.com>
2024-05-01 22:37:26 +0600

    function in typescript

---
+++ module1/dist/1.5.js
@@ -0,0 +1,9 @@
+"use strict";
+// Reference Type --> Object
+const user = {
+    company: "Programing Hero",
+    firstName: "Mezbaul",
+    lastName: "Abedin",
+    isMarried: true,
+};
+// user.company = 'PH' // because its a read only property
---
+++ module1/dist/1.6.js
@@ -0,0 +1,9 @@
+"use strict";
+// Learning function
+// Normal function
+// Arrow Function
+function add(num1, num2 = 10) {
+    return num1 + num2;
+}
+;
+add(2, 5);

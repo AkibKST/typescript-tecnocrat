c6be724f50d1b464b26995d8cd7215248d89d9ba
AkibKST <akibkst22@gmail.com>
2024-05-07 09:55:53 +0600

    Learn static in OOP

---
+++ module3/3.7.ts
@@ -0,0 +1,24 @@
+{
+    // statics in OOP
+
+    class Counter {
+       static count: number = 0;
+        static increment(){
+           return (Counter.count = Counter.count + 1);
+        }
+
+        static decrement(){
+           return (Counter.count = Counter.count - 1);
+        }
+    }
+
+    // const instance1 = new Counter();
+    console.log(Counter.increment());  // 1  -> different memory
+    // console.log(instance1.increment());  // 2  -> || memory
+    // console.log(instance1.increment());  // 3  -> || memory
+
+    // const instance2 = new Counter();
+    console.log(Counter.increment());  // 1  -> different memory
+    // console.log(instance2.increment());  // 2  -> || memory
+    // 
+}
